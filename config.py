6cc289b5fcca2329d036ba9d0a0a2358370a9156
Cheuk Shu Ho <72436714+2246250622@users.noreply.github.com>
2024-02-19 15:47:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
-api_key = "3QD3j49G+vTc7wRZtCT/rmzg7BRsU2+YKhaH/T2GzGg="
-api_secret = "mJY3AcHir/jhwxEnd/de+gj6ENJ9d24fMmQNrsJ8zv8="
+api_key = "你的api_key"
+api_secret = "你的api_secret"
 
 trade_quantity = 10 # 交易的下單數量.
 vol_precision = 2 # 數量精度， 保留兩位小數.
