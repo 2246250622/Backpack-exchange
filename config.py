dd3559417f2e1cafc1bacfea077c717530dc8d01
Cheuk Shu Ho <72436714+2246250622@users.noreply.github.com>
2024-02-19 15:46:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 api_key = "3QD3j49G+vTc7wRZtCT/rmzg7BRsU2+YKhaH/T2GzGg="
 api_secret = "mJY3AcHir/jhwxEnd/de+gj6ENJ9d24fMmQNrsJ8zv8="
 
-trade_quantity = 10  # 交易的下单数量.
-vol_precision = 2  # 数量精度， 保留两位小数.
-trading_fee = 0.001  # 千分之手续费.
-trade_pair = "SOL_USDC"  # 交易对.
+trade_quantity = 10 # 交易的下單數量.
+vol_precision = 2 # 數量精度， 保留兩位小數.
+trading_fee = 0.001 # 千分之手續費.
+trade_pair = "SOL_USDC" # 交易對.
